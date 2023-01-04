5dbbf84f993e9014e0e068093234f3dbe5b8475f
Brack-Wang <wfr2099@gmail.com>
2023-01-04 11:38:48 -0600

    new

--- src/app.tsx
+++ src/app.tsx
@@ -23,76 +23,98 @@ export default function App({ req, isCold }) {
               <span>You're not defined by what you had, but by what you want to be.</span>
             </h3>
             <div class="format">
-            <div class="info">
-
-              <div class="block">
-                <div class="contents">
-                  <span class="details">{' '}
-                    <a target="_blank" href="https://github.com/Brack-Wang?tab=repositories" color="red">
-                      Github
-                    </a> 
-                  </span>
-                  <strong class="details">{' '}
-                    <a target="_blank" href="https://space.bilibili.com/13924169">
-                      BiliBili
-                    </a> 
-                  </strong>
+              <div class="info">
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://github.com/Brack-Wang?tab=repositories" color="red">
+                        Github
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://space.bilibili.com/13924169">
+                        BiliBili
+                      </a> 
+                    </strong>
+                  </div>
                 </div>
-              </div>
-
-              <div class="block">
-                <div class="contents">
-                  <span class="details">{' '}
-                    <a target="_blank" href="https://www.youtube.com/channel/UCBnlGtn9RYqbfJYIM5nwUKg">
-                      Youtube
-                    </a> 
-                  </span>
-                  <strong class="details">{' '}
-                    <a target="_blank" href="https://www.zhihu.com/people/yang-qi-lin-49/posts">
-                      Zhihu
-                    </a> 
-                  </strong>
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://www.youtube.com/channel/UCBnlGtn9RYqbfJYIM5nwUKg">
+                        Youtube
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://www.zhihu.com/people/yang-qi-lin-49/posts">
+                        Zhihu
+                      </a> 
+                    </strong>
+                  </div>
                 </div>
-              </div>
 
-              <div class="block">
-                <div class="contents">
-                  <span class="details">{' '}
-                    <a target="_blank" href="https://ietresearch.onlinelibrary.wiley.com/doi/full/10.1049/cvi2.12112">
-                        Publication
-                    </a> 
-                  </span>
-                  <strong class="details">{' '}
-                    <a target="_blank" href="https://franks-life-lv963oheh-brack-wang.vercel.app/">
-                        Life
-                    </a> 
-                  </strong>
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://ietresearch.onlinelibrary.wiley.com/doi/full/10.1049/cvi2.12112">
+                          Publication
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://franks-life-lv963oheh-brack-wang.vercel.app/">
+                          Life
+                      </a> 
+                    </strong>
+                  </div>
+                </div>            
+              </div>
+              <div class="info2">
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://github.com/Brack-Wang?tab=repositories" color="red">
+                        Github
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://space.bilibili.com/13924169">
+                        BiliBili
+                      </a> 
+                    </strong>
+                  </div>
                 </div>
-              </div>
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://www.youtube.com/channel/UCBnlGtn9RYqbfJYIM5nwUKg">
+                        Youtube
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://www.zhihu.com/people/yang-qi-lin-49/posts">
+                        Zhihu
+                      </a> 
+                    </strong>
+                  </div>
+                </div>
 
+                <div class="block">
+                  <div class="contents">
+                    <span class="details">{' '}
+                      <a target="_blank" href="https://ietresearch.onlinelibrary.wiley.com/doi/full/10.1049/cvi2.12112">
+                          Publication
+                      </a> 
+                    </span>
+                    <strong class="details">{' '}
+                      <a target="_blank" href="https://franks-life-lv963oheh-brack-wang.vercel.app/">
+                          Life
+                      </a> 
+                    </strong>
+                  </div>
+                </div>            
+              </div>
             </div>
-            </div>
 
-            {/* <div class="info">
-
-              <div class="block">
-                <div class="contents">
-                  <span class="details">{' '}
-                    <a target="_blank" href="https://github.com/Brack-Wang?tab=repositories" color="red">
-                      Github
-                    </a> 
-                  </span>
-                  <strong class="details">{' '}
-                    <a target="_blank" href="https://space.bilibili.com/13924169">
-                      BiliBili
-                    </a> 
-                  </strong>
-                </div>
-              </div>
-              </div> */}
-
-
-          
           </main>
           <div class="debug">
             Now is {new Date().toLocaleDateString()} 
